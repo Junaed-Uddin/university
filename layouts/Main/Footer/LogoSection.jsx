2a798d17d92da0b6811d60696dd5b73c983b6cc3
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-12 02:10:34 +0600

    Footer logo part added

--- layouts/Main/Footer/LogoSection.jsx
+++ layouts/Main/Footer/LogoSection.jsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
+import {
+  BsInstagram as InstaIcon,
+  BsTwitter as TwitterIcon,
+} from "react-icons/bs";
+import { CgFacebook as FbIcon } from "react-icons/cg";
 
 function LogoSection() {
   return (
     <div className="col-start-1 col-end-13">
       <Link href="/">
-        <div className="font-bold text-xl">
+        <div className="font-bold text-2xl leading-6">
           <span className="text-blue-500">KingStar</span>
           <span> University</span>
         </div>
-        <em className="text-sm text-gray-800">Science 2010</em>
+        <em className="text-sm text-gray-800 font-semibold">Science 2010</em>
       </Link>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minima
@@ -17,11 +22,25 @@ function LogoSection() {
       </p>
 
       {/* Social Section */}
-      <div className="flex items-center gap-2">
-        <Link href=""></Link>
-        <Link href=""></Link>
-        <Link href=""></Link>
-        <Link href=""></Link>
+      <div className="flex items-center gap-2 text-2xl my-3">
+        <Link
+          href=""
+          className="p-2 rounded-md hover:bg-primary/20 hover:text-primary transition"
+        >
+          <FbIcon />
+        </Link>
+        <Link
+          href=""
+          className="p-2 rounded-md hover:bg-primary/20 hover:text-primary transition"
+        >
+          <TwitterIcon />
+        </Link>
+        <Link
+          href=""
+          className="p-2 rounded-md hover:bg-primary/20 hover:text-primary transition"
+        >
+          <InstaIcon />
+        </Link>
       </div>
     </div>
   );
