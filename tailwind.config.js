c0c3d385e80cd7c984c422b4adde4e99b5a78e4f
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-11 13:27:03 +0600

    color palette implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,26 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1a68db",
+        "primary-hover": "#2772e3",
+        "on-primary": "#fff",
+        secondary: "#fc8d2b",
+        "secondary-hover": "#f59c4e",
+        "on-secondary": "#fff",
+        "text-primary": "#000",
+        "text-secondary": "#1c1c1c",
+        "bg-main": "#f0f2f5",
+        paper: "#ffffff",
+        "ol-default": "rgba(0,0,0,0.15)",
+      },
+    },
   },
   plugins: [],
-}
+};
