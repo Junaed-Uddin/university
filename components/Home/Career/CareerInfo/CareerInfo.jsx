2d6bb1e493e45e28b8fa3d424f10083f36b5bfe1
Junaed-Uddin <junaed.mohammed190@gmail.com>
2023-02-12 23:55:27 +0600

    animation added

--- components/Home/Career/CareerInfo/CareerInfo.jsx
+++ components/Home/Career/CareerInfo/CareerInfo.jsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import React from "react";
 import { HiArrowSmRight } from "react-icons/hi";
+import styles from "../../../../styles/career.module.css";
 
 const CareerInfo = ({ careerInfo }) => {
   const { heading, imageURL, description, buttonName } = careerInfo;
   return (
     <div className="w-full flex flex-col justify-center items-center">
-      <Image
-        className="rounded-full object-cover w-[300px] h-[300px]"
-        src={imageURL}
-        alt="career-facilities"
-        quality={100}
-      />
+      <div className={`${styles.imgHover} img`}>
+        <Image
+          className="rounded-full object-cover w-[300px] h-[300px]"
+          src={imageURL}
+          alt="career-facilities"
+          quality={100}
+        />
+      </div>
       <div className="mt-10 w-full text-center max-w-[350px]">
         <h2 className="text-2xl font-bold mb-3">{heading}</h2>
         <p className="mb-3 h-20">{description}</p>
