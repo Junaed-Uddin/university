b66eeb1449a19587d70743b463e09932175d84ab
Resalat07 <resalatefti@gmail.com>
2023-02-13 07:10:23 -0800

    program and faculty 3

--- components/Home/Faculty/index.jsx
+++ components/Home/Faculty/index.jsx
@@ -1,7 +1,7 @@
 import styles from "@/styles/faculty.module.css";
 const Faculty = () => {
     return (
-        <div className=" text-center bg-background p-10">
+        <div className=" text-center bg-background p-10 mb-5">
             <p>EVERYTHING YOU NEED</p>
             <div className="relative">
                 <h2 className={`text-4xl text-txt-primary text-center font-bold relative`}>
@@ -15,14 +15,14 @@ const Faculty = () => {
             <div className=" flex justify-center mt-6">
 
                 <div className="grid  lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1    gap-7 ">
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>CSE</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>EEE</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>CIVIL</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>ENGLISH</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>DBA</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>LIBRARY</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>LAW</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-red-600 w-72 p-3 `}>MECHANICS</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>CSE</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>EEE</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>CIVIL</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>ENGLISH</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>DBA</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>LIBRARY</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>LAW</button>
+                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-72 p-3 `}>MECHANICS</button>
 
                 </div>
 
--- components/Home/Programs/index.jsx
+++ components/Home/Programs/index.jsx
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import chose from "../../../public/assets/images/programChose/program.png"
-import { BiTimer  } from 'react-icons/bi';
-import { BsFillFileEarmarkArrowDownFill ,BsLayersFill  } from 'react-icons/bs';
-import {SlBadge } from 'react-icons/sl';
+import { FaUserGraduate } from 'react-icons/fa';
+import { GiArchiveResearch} from 'react-icons/gi';
+import { GiTeacher } from 'react-icons/gi';
+import { GiRank3} from 'react-icons/gi';
+import styles from "@/styles/program.module.css";
 
 
 const Programs = () => {
     return (
         <div className=" mt-10 bg-background   pb-10 mx-3">
+
+            <div className="relative">
+                <h2 className={`text-4xl text-txt-primary text-center font-bold relative`}>
+                Why You Choose <br /> Our Program
+                    <span className={`${styles.textUnderline}`}></span>
+                </h2>
+                
+            </div>
+            <div className="  flex justify-center">
+            <p className=" mt-4 p-3 text-center max-w-[700px]">The main purpose of the us , Is to provide an environment In which faculty and students Can discover and transmit The knowledge, wisdom, and values. </p>
+
+            </div>
+            
 
             <div>
                 <div className=" flex justify-center pt-10 ">
@@ -15,9 +30,9 @@ const Programs = () => {
                         <div className=" lg:col-span-2 pb-10">
                             <div className=" grid lg:grid-cols-2 md:grid-cols-2 max-w-[660px] gap-3  h-60 shadow-2xl p-3 rounded-lg bg-white ">
                                 <div className=" pl-3 ">
-                                    <h1 className=" text-3xl font-bold pt-3">Why You Choose <br /> Our Program</h1>
+                                    <h1 className=" text-3xl font-bold pt-3">Start Chat with Us</h1>
                                     <p className=" pt-2 text-sm">I was design to assist users with a wide range of texts and answer questions to the best of my ability</p>
-                                    <button className=" p-3 mt-3 bg-violet-400  text-white rounded-lg">Learn More</button>
+                                    <button className=" p-3 mt-3 bg-blue-500 shadow-lg shadow-blue-400 text-white rounded-lg">Start Chat</button>
                                 </div>
                                 <div className=" flex justify-center p-6  ml-2 lg:visible  md:visible invisible">
                                     <Image
@@ -32,9 +47,9 @@ const Programs = () => {
                         <div className=" flex justify-center mb-6">
                             <div className=" w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500 ">
 
-                                <BiTimer className=" text-white text-5xl bg-cyan-400 shadow-lg shadow-cyan-500/50 p-2 rounded-lg"></BiTimer>
-                                <h3 className=" mt-4 text-lg font-bold">Flexible Time</h3>
-                                <p className=" mt-3 text-sm">Flexible time, also known as flextime or flexi-time, refers to a work schedule.</p>
+                                <FaUserGraduate className=" text-white text-5xl bg-cyan-400 shadow-lg shadow-cyan-500/50 p-2 rounded-lg"></FaUserGraduate>
+                                <h3 className=" mt-4 text-lg font-bold">Graduate Education</h3>
+                                <p className=" mt-3 text-sm">We offer Undergraduate and Postgraduate education <a className=" text-blue-500  cursor-pointer">see more</a></p>
 
                             </div>
                         </div>
@@ -46,27 +61,27 @@ const Programs = () => {
                         <>
                             <div className="  w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500">
 
-                                <BsFillFileEarmarkArrowDownFill className=" text-white text-5xl bg-violet-400 shadow-lg shadow-violet-400 p-2 rounded-lg"></BsFillFileEarmarkArrowDownFill>
-                                <h3 className=" mt-4 text-lg font-bold">Access Any Where</h3>
-                                <p className=" mt-3 text-sm">Flexible time, also known as flextime or flexi-time, refers to a work schedule.</p>
+                                <GiArchiveResearch className=" text-white text-5xl bg-violet-400 shadow-lg shadow-violet-400 p-2 rounded-lg"></GiArchiveResearch>
+                                <h3 className=" mt-4 text-lg font-bold">Research</h3>
+                                <p className=" mt-3 text-sm">We provide wide range of research materials.<a className=" text-blue-500  cursor-pointer">see more</a></p>
 
                             </div>
                         </>
                         <>
                             <div className="  w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500">
 
-                                <BsLayersFill className=" text-white text-5xl bg-red-400 p-2 rounded-lg shadow-lg shadow-red-400"></BsLayersFill>
-                                <h3 className=" mt-4 text-lg font-bold">Organize Program</h3>
-                                <p className=" mt-3 text-sm">Flexible time, also known as flextime or flexi-time, refers to a work schedule.</p>
+                                <GiTeacher className=" text-white text-5xl bg-red-400 p-2 rounded-lg shadow-lg shadow-red-400"></GiTeacher>
+                                <h3 className=" mt-4 text-lg font-bold">Graduate & Professional</h3>
+                                <p className=" mt-3 text-sm">We have arrange workshops to make students more professional.<a className=" text-blue-500  cursor-pointer">see more</a> </p>
 
                             </div>
                         </>
                         <>
                             <div className="  w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500">
 
-                                <SlBadge className=" text-white text-5xl bg-amber-400 shadow-lg shadow-amber-400 p-2 rounded-lg"></SlBadge>
-                                <h3 className=" mt-4 text-lg font-bold">Certificate</h3>
-                                <p className=" mt-3 text-sm">Flexible time, also known as flextime or flexi-time, refers to a work schedule.</p>
+                                <GiRank3 className=" text-white text-5xl bg-amber-400 shadow-lg shadow-amber-400 p-2 rounded-lg"></GiRank3>
+                                <h3 className=" mt-4 text-lg font-bold">Rankings</h3>
+                                <p className=" mt-3 text-sm">We made top 5 in last year. <a className=" text-blue-500  cursor-pointer">see more</a></p>
 
                             </div>
                         </>
