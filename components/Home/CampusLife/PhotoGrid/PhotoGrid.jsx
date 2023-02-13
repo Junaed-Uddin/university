10f4e3730c66f50c4ca24b94d4c7690260370222
tausifabid12 <tasifabid12@gmail.com>
2023-02-13 14:35:41 +0600

    gap problem fixed and image looping enabled

--- components/Home/CampusLife/PhotoGrid/PhotoGrid.jsx
+++ components/Home/CampusLife/PhotoGrid/PhotoGrid.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 
 const PhotoGrid = ({ img1, img2, img3 }) => {
   return (
-    <div className="w-full lg:w-11/12  min-h-[300px] lg:min-h-[500px] grid grid-rows-1 lg:grid-rows-2 grid-cols-1 lg:grid-cols-3 gap-4">
+    <div className="w-full lg:w-11/12  min-h-[300px] lg:min-h-[500px] grid grid-rows-1 lg:grid-rows-2 grid-cols-1 lg:grid-cols-3 gap-5">
       <div className=" row-span-2 col-span-2">
         <Image
           src={img1}
           width={800}
           height={800}
           alt="imag"
-          className="w-full h-full lg:h-[550px] "
+          objectFit="cover"
+          className="w-full h-full lg:h-[500px] "
         />
       </div>
       <div className=" hidden lg:block">
@@ -19,6 +20,7 @@ const PhotoGrid = ({ img1, img2, img3 }) => {
           width={650}
           height={600}
           alt="imag"
+          objectFit="cover"
           className="w-full h-full"
         />
       </div>
@@ -29,6 +31,7 @@ const PhotoGrid = ({ img1, img2, img3 }) => {
           height={600}
           alt="imag"
           className="w-full h-full"
+          objectFit="cover"
         />
       </div>
     </div>
