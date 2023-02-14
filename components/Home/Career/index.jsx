98e209ef5159af7233626895d77b7b817e1293f3
Junaed-Uddin <junaed.mohammed190@gmail.com>
2023-02-14 13:07:55 +0600

    some styles changes

--- components/Home/Career/index.jsx
+++ components/Home/Career/index.jsx
@@ -33,7 +33,7 @@ const Career = () => {
   ];
 
   return (
-    <section className="my-10">
+    <section className="mb-10 mt-10 sm:mt-0">
       <div className="relative">
         <h2
           className={`text-4xl text-txt-primary text-center font-display font-bold relative`}
