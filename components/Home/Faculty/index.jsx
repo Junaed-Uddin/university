874ac62a0e2122319bbfe440febc193e4eb64570
Resalat07 <resalatefti@gmail.com>
2023-02-15 07:57:51 -0800

    program and faculty 7

--- components/Home/Faculty/index.jsx
+++ components/Home/Faculty/index.jsx
@@ -14,15 +14,120 @@ const Faculty = () => {
             <p>Use these quick links to find the systems and sites need most.</p>
             <div className=" flex justify-center mt-6">
 
-                <div className="grid  lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1    gap-7 ">
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>CSE</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>EEE</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>CIVIL</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>ENGLISH</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>DBA</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>LIBRARY</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>LAW</button>
-                    <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>MECHANICS</button>
+                <div className="grid  lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1    gap-7 ">
+                    <div>
+                        <div className=" w-64 ">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/6755058/pexels-photo-6755058.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-56 p-3 `}>Computer Science</button>
+
+                        </div>
+                    </div>
+
+
+
+             
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/3672355/pexels-photo-3672355.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>Electrical Engineering</button>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/834892/pexels-photo-834892.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>Civil Engineering</button>
+
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/7777182/pexels-photo-7777182.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>ENGLISH</button>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`}  src="https://images.pexels.com/photos/3184292/pexels-photo-3184292.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>DBA</button>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/8112199/pexels-photo-8112199.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>LAW</button>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/414579/pexels-photo-414579.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>MECHANICS</button>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/714698/pexels-photo-714698.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>Physics</button>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className=" w-64">
+                            <img className={`w-64 h-40 ${styles.imgShadow}`} src="https://images.pexels.com/photos/5726837/pexels-photo-5726837.jpeg?auto=compress&cs=tinysrgb&w=1600" alt=" " />
+
+                        </div>
+                        <div className=" -translate-y-5">
+
+                        <button className={`${styles.opt} ${styles.optHover} text-white bg-blue-600 w-60 p-3 `}>Chemistry</button>
+
+                        </div>
+                    </div>
+                    
+                    
+                    
+                    
+                    
+              
+                    
+                    
 
                 </div>
 
--- components/Home/Programs/index.jsx
+++ components/Home/Programs/index.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import chose from "../../../public/assets/images/programChose/program.png"
 import { FaUserGraduate } from 'react-icons/fa';
-import { GiArchiveResearch} from 'react-icons/gi';
+import { GiArchiveResearch } from 'react-icons/gi';
 import { GiTeacher } from 'react-icons/gi';
-import { GiRank3} from 'react-icons/gi';
+import { GiRank3 } from 'react-icons/gi';
 import styles from "@/styles/program.module.css";
 
 
@@ -13,28 +13,28 @@ const Programs = () => {
 
             <div className="relative">
                 <h2 className={`text-4xl text-txt-primary text-center font-bold relative`}>
-                Why You Choose <br /> Our Program
+                    Why You Choose <br /> Our Program
                     <span className={`${styles.textUnderline}`}></span>
                 </h2>
-                
-            </div>
-            <div className="  flex justify-center">
-            <p className=" mt-4 p-3 text-center max-w-[700px]">The main purpose of the us , Is to provide an environment In which faculty and students Can discover and transmit The knowledge, wisdom, and values. </p>
 
             </div>
-            
+            <div className="  flex justify-center">
+                <p className=" mt-4 p-3 text-center max-w-[700px]">The main purpose of the us , Is to provide an environment In which faculty and students Can discover and transmit The knowledge, wisdom, and values. </p>
+
+            </div>
+
 
             <div>
                 <div className=" flex justify-center pt-10 ">
                     <div className="  grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 ">
-                        <div className=" lg:col-span-2 pb-10">
-                            <div className=" grid lg:grid-cols-2  max-w-[660px] gap-3  h-60 shadow-2xl p-3 rounded-lg bg-white ">
+                        <div className=" lg:col-span-2 md:col-span-2 pb-10">
+                            <div className=" grid lg:grid-cols-2 md:grid-cols-2 max-w-[660px] gap-3  h-60 shadow-2xl p-3 rounded-lg bg-white ">
                                 <div className=" pl-3 ">
                                     <h1 className=" text-3xl font-bold pt-3">Start Chat with Us</h1>
                                     <p className=" pt-2 text-sm">I was design to assist users with a wide range of texts and answer questions to the best of my ability</p>
                                     <button className=" p-3 mt-3 bg-blue-500 shadow-lg shadow-blue-400 text-white rounded-lg">Start Chat</button>
                                 </div>
-                                <div className=" flex justify-center p-6  ml-2 lg:visible   invisible">
+                                <div className=" flex justify-center p-6  ml-2 lg:visible md:visible  invisible">
                                     <Image
                                         src={chose}
                                         alt=''
@@ -53,12 +53,12 @@ const Programs = () => {
 
                             </div>
                         </div>
-                    </div>
-                </div>
 
-                <div className=" flex justify-center">
-                    <div className=" grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6">
-                        <>
+
+                        {/* <div className=" flex justify-center"> */}
+                        {/* <div className=" grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6"> */}
+
+                        <div className=" flex justify-center mb-6">
                             <div className="  w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500">
 
                                 <GiArchiveResearch className=" text-white text-5xl bg-violet-400 shadow-lg shadow-violet-400 p-2 rounded-lg"></GiArchiveResearch>
@@ -66,8 +66,10 @@ const Programs = () => {
                                 <p className=" mt-3 text-sm">We provide wide range of research materials.<a className=" text-blue-500  cursor-pointer">see more</a></p>
 
                             </div>
-                        </>
-                        <>
+                        </div>
+
+
+                        <div className=" flex justify-center mb-6">
                             <div className="  w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500">
 
                                 <GiTeacher className=" text-white text-5xl bg-red-400 p-2 rounded-lg shadow-lg shadow-red-400"></GiTeacher>
@@ -75,8 +77,9 @@ const Programs = () => {
                                 <p className=" mt-3 text-sm">We have arrange workshops to make students more professional.<a className=" text-blue-500  cursor-pointer">see more</a> </p>
 
                             </div>
-                        </>
-                        <>
+                        </div>
+
+                        <div className=" flex justify-center mb-6">
                             <div className="  w-80 h-60 rounded-lg  backdrop-blur-xl bg-white/50 p-8 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none hover:shadow-xl ease-in-out duration-500">
 
                                 <GiRank3 className=" text-white text-5xl bg-amber-400 shadow-lg shadow-amber-400 p-2 rounded-lg"></GiRank3>
@@ -84,9 +87,13 @@ const Programs = () => {
                                 <p className=" mt-3 text-sm">We made top 5 in last year. <a className=" text-blue-500  cursor-pointer">see more</a></p>
 
                             </div>
-                        </>
+                        </div>
+
                     </div>
                 </div>
+
+                {/* </div> */}
+                {/* </div> */}
             </div>
         </div>
     );
