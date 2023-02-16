768ec32ead908a5515a784b948334d6cb9308590
Junaed-Uddin <junaed.mohammed190@gmail.com>
2023-02-16 10:58:45 +0600

    newsletter updated

--- components/Home/NewsLetter/index.jsx
+++ components/Home/NewsLetter/index.jsx
@@ -4,17 +4,17 @@ import { HiArrowSmRight } from "react-icons/hi";
 const NewsLetter = () => {
   return (
     <section className="mt-10">
-      <div className=" h-72 flex flex-col justify-center items-center ">
-        <h2 className="text-4xl text-center max-w-[400px] leading-snug text-on-secondary font-bold">
+      <div className=" h-72 flex flex-col justify-center items-center mx-3">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl text-center font-sans max-w-[450px] text-on-secondary tracking-wider font-bold">
           Get the latest news delivered to you inbox
         </h2>
-        <div className="flex justify-center items-center w-full mt-5">
+        <div className="flex flex-wrap gap-2 sm:gap-0 justify-center items-center w-full mt-8">
           <input
-            className="h-11 w-2/6 pl-4"
+            className="h-11 w-[440px] sm:w-[450px] pl-4 outline-none border-none"
             type="text"
             placeholder="Your Email"
           />
-          <button className="flex justify-center items-center gap-2 text-on-primary bg-primary h-11 px-8 text-md">
+          <button className="flex w-[160px] md:w-[180px] justify-center items-center gap-2 text-on-primary bg-primary h-11 text-md">
             <span>Subscribe</span>
             <HiArrowSmRight size={18}></HiArrowSmRight>
           </button>
